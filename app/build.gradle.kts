6e21aa190c851f1ad574bdb319f6a893b1ed25f7
immortal-forest <ali.salman.sa.01@gmail.com>
2024-02-26 11:37:07 +0545

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "xyz.immortalforest.widgets"
         minSdk = 31
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
