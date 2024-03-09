6e6edca4b201c4ce4d76c0a6c45590594d0f77ba
immortal-forest <ali.salman.sa.01@gmail.com>
2024-03-09 12:13:36 +0545

    update WTrack

--- app/src/main/java/xyz/immortalforest/widgets/widget/models/WTrack.kt
+++ app/src/main/java/xyz/immortalforest/widgets/widget/models/WTrack.kt
@@ -5,31 +5,26 @@ import com.spotify.protocol.types.Artist
 import com.spotify.protocol.types.ImageUri
 import com.spotify.protocol.types.Track
 
-class WTrack(
-    val name: String,
-    val uri: String,
-    val imageUri: ImageUri,
-    val artists: List<Artist>,
-    val album: Album,
-) {
-    constructor(
-        name: String,
-    ) : this(
-        name,
-        "",
-        ImageUri(""),
-        emptyList(),
-        Album("", "")
-    )
+class WTrack {
+    var name: String = "empty"
+    var uri: String = ""
+    var imageUri: ImageUri = ImageUri("")
+    var artists: List<Artist> = emptyList()
+    var album: Album = Album("", "")
 
-    constructor(
-        track: Track
-    ) : this(
-        track.name,
-        track.uri,
-        track.imageUri,
-        track.artists.toList(),
-        track.album
-    )
+    fun createTrack(track: Track): WTrack {
+        this.name = track.name
+        this.uri = track.uri
+        this.imageUri = track.imageUri
+        this.artists = track.artists
+        this.album = track.album
+        return this
+    }
+
+    fun getArtists(): String {
+        return artists.joinToString(", ") {
+            it.name
+        }
+    }
 
 }
