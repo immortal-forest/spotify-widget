397e463c9271db3261945a0a0a3a437d6e748b70
immortal-forest <ali.salman.sa.01@gmail.com>
2024-03-09 12:14:53 +0545

    add Medium Widget with dynamic colors

--- app/src/main/java/xyz/immortalforest/widgets/widget/MediumWidget.kt
+++ app/src/main/java/xyz/immortalforest/widgets/widget/MediumWidget.kt
@@ -1,12 +1,20 @@
 package xyz.immortalforest.widgets.widget
 
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.graphics.Color
+import androidx.core.content.ContextCompat
 import androidx.glance.GlanceId
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
+import androidx.glance.appwidget.SizeMode
 import androidx.glance.appwidget.provideContent
+import androidx.palette.graphics.Palette
 import com.spotify.android.appremote.api.SpotifyAppRemote
+import com.spotify.protocol.types.ImageUri
 import com.spotify.protocol.types.PlayerRestrictions
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -14,6 +22,7 @@ import kotlinx.coroutines.launch
 import xyz.immortalforest.widgets.widget.models.WImage
 import xyz.immortalforest.widgets.widget.models.WTrack
 import xyz.immortalforest.widgets.widget.presentation.Loading
+import xyz.immortalforest.widgets.widget.presentation.MediumContent
 
 class MediumWidgetReceiver : GlanceAppWidgetReceiver() {
     override val glanceAppWidget: GlanceAppWidget
@@ -24,7 +33,7 @@ class MediumWidgetReceiver : GlanceAppWidgetReceiver() {
 
 class MediumWidget : GlanceAppWidget() {
 
-    // spotify
+    // spotify stuff
     private val spotifyHelper = SpotifyHelper()
     private var spotifyAppRemote: SpotifyAppRemote? = null
 
@@ -33,11 +42,15 @@ class MediumWidget : GlanceAppWidget() {
     private val paused = mutableStateOf(false)
     private val image = mutableStateOf(WImage())
     private val playerRestrictions = mutableStateOf(PlayerRestrictions.DEFAULT)
-    private val track = mutableStateOf(WTrack(""))
+    private val track = mutableStateOf(WTrack())
+    private val defaultContainerColor = (0xFF888888).toInt()
 
+    private val containerColor = mutableStateOf(Color(defaultContainerColor).copy(alpha = 0.5f))
+    private val containerTextColor = mutableStateOf(Color.Black)
+    private val iconColor = mutableStateOf(Color.Black)
 
-//    override val sizeMode: SizeMode
-//        get() = SizeMode.Exact
+    override val sizeMode: SizeMode
+        get() = SizeMode.Exact
 
     override suspend fun provideGlance(context: Context, id: GlanceId) {
         spotifyHelper.new(context, id)
@@ -47,7 +60,34 @@ class MediumWidget : GlanceAppWidget() {
             if (!loading.value && spotifyAppRemote == null) {
                 Loading()
             } else {
-                TODO("MediumWidgetContent()")
+                MediumContent(
+                    spotifyHelper = spotifyHelper,
+                    loading = loading.value,
+                    playerRestrictions = playerRestrictions.value,
+                    imageBitmap = image.value.bitmap,
+                    isPaused = paused.value,
+                    containerColor = containerColor.value,
+                    containerTextColor = containerTextColor.value,
+                    iconColor = iconColor.value,
+                    track = track.value,
+                    {
+                        ContextCompat.startActivity(
+                            context,
+                            Intent(
+                                Intent.ACTION_VIEW,
+                                Uri.parse("spotify:track:${track.value.uri}")
+                            )
+                                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK),
+                            Bundle.EMPTY
+                        )
+                        loadImage(image.value.uri)
+                    },
+                    { context, id ->
+                        updateUI(context, id)
+                    }
+                ) {
+                    connectToSpotify()
+                }
             }
         }
     }
@@ -58,6 +98,11 @@ class MediumWidget : GlanceAppWidget() {
         super.onDelete(context, glanceId)
     }
 
+    private fun updateUI(context: Context, glanceId: GlanceId) {
+        CoroutineScope(Dispatchers.IO).launch {
+            update(context, glanceId)
+        }
+    }
 
     private fun connectToSpotify() {
         spotifyHelper.connectToSpotify(
@@ -80,26 +125,40 @@ class MediumWidget : GlanceAppWidget() {
         }
         spotifyAppRemote!!.playerApi.subscribeToPlayerState().setEventCallback { playerState ->
             paused.value = playerState.isPaused
+            if (track.value.uri != playerState.track.uri) {
+                track.value = WTrack().createTrack(playerState.track)
+                updateUI(context, id)
+            }
             if (image.value.uri != playerState.track.imageUri) {
                 spotifyAppRemote!!.imagesApi.getImage(playerState.track.imageUri)
                     .setResultCallback { bitmap ->
                         image.value = WImage(playerState.track.imageUri, bitmap)
+                        CoroutineScope(Dispatchers.IO).launch {
+                            Palette.from(bitmap).generate().let { palette ->
+                                containerColor.value =
+                                    (palette.lightVibrantSwatch?.rgb?.let { Color(it) }
+                                        ?: Color(defaultContainerColor)).copy(
+                                        alpha = 0.8f
+                                    )
+                                containerTextColor.value =
+                                    (palette.lightVibrantSwatch?.titleTextColor?.let { Color(it) }
+                                        ?: Color.Black)
+                                iconColor.value =
+                                    (palette.darkVibrantSwatch?.rgb?.let { Color(it) }
+                                        ?: Color.Black)
+                            }
+                            updateUI(context, id)
+                        }
                     }
             }
-            if (playerRestrictions.value != playerState.playbackRestrictions) {
-                playerRestrictions.value = playerState.playbackRestrictions
-            }
-            if (track.value.uri != playerState.track.uri) {
-                track.value = WTrack(playerState.track)
-            }
+            playerRestrictions.value = playerState.playbackRestrictions
             updateUI(context, id)
         }
     }
 
-
-    private fun updateUI(context: Context, glanceId: GlanceId) {
-        CoroutineScope(Dispatchers.IO).launch {
-            update(context, glanceId)
+    private fun loadImage(imageUri: ImageUri) {
+        spotifyAppRemote!!.imagesApi.getImage(imageUri).setResultCallback {
+            image.value = WImage(imageUri, it)
         }
     }
 
---
+++ app/src/main/java/xyz/immortalforest/widgets/widget/presentation/MediumWidgetContent.kt
@@ -0,0 +1,222 @@
+package xyz.immortalforest.widgets.widget.presentation
+
+import android.content.Context
+import android.graphics.Bitmap
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.glance.ColorFilter
+import androidx.glance.GlanceId
+import androidx.glance.GlanceModifier
+import androidx.glance.Image
+import androidx.glance.ImageProvider
+import androidx.glance.action.clickable
+import androidx.glance.appwidget.cornerRadius
+import androidx.glance.background
+import androidx.glance.layout.Alignment
+import androidx.glance.layout.Box
+import androidx.glance.layout.Column
+import androidx.glance.layout.ContentScale
+import androidx.glance.layout.Row
+import androidx.glance.layout.Spacer
+import androidx.glance.layout.fillMaxSize
+import androidx.glance.layout.padding
+import androidx.glance.layout.size
+import androidx.glance.layout.wrapContentSize
+import androidx.glance.text.FontFamily
+import androidx.glance.text.FontStyle
+import androidx.glance.text.FontWeight
+import androidx.glance.text.Text
+import androidx.glance.text.TextAlign
+import androidx.glance.text.TextDecoration
+import androidx.glance.text.TextStyle
+import androidx.glance.unit.ColorProvider
+import com.spotify.protocol.types.PlayerRestrictions
+import xyz.immortalforest.widgets.R
+import xyz.immortalforest.widgets.widget.SpotifyHelper
+import xyz.immortalforest.widgets.widget.models.WTrack
+
+@Composable
+fun MediumContent(
+    spotifyHelper: SpotifyHelper,
+    loading: Boolean,
+    playerRestrictions: PlayerRestrictions,
+    imageBitmap: Bitmap,
+    isPaused: Boolean,
+    containerColor: Color,
+    containerTextColor: Color,
+    iconColor: Color,
+    track: WTrack,
+    imageClick: () -> Unit,
+    onSuccessAction: (Context, GlanceId) -> Unit,
+    onErrorAction: () -> Unit,
+) {
+    Box(
+        modifier = GlanceModifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Box(
+            modifier = GlanceModifier
+                .cornerRadius(16.dp)
+                .background(containerColor)
+                .padding(4.dp)
+                .wrapContentSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Box(
+                modifier = GlanceModifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Row(
+                    modifier = GlanceModifier.fillMaxSize()
+                ) {
+
+                    Box(
+                        modifier = GlanceModifier
+                            .size(89.dp, 95.dp)
+                            .cornerRadius(12.dp),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Image(
+                            provider = ImageProvider(imageBitmap),
+                            contentDescription = "Album art",
+                            contentScale = ContentScale.FillBounds,
+                            modifier = GlanceModifier.fillMaxSize()
+                                .clickable(imageClick)
+                        )
+                    }
+//                    Spacer(
+//                        modifier = GlanceModifier.size(12.dp, 0.dp)
+//                    )
+                    Column(
+                        modifier = GlanceModifier.fillMaxSize()
+                            .padding(end = 12.dp, top = 5.dp),
+                        verticalAlignment = Alignment.Top,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        val trackNameLen = track.name.length
+                        val artistsNameLen = track.getArtists().length
+
+                        Text(
+                            text = track.name,
+                            maxLines = 1,
+                            style = TextStyle(
+                                color = ColorProvider(containerTextColor),
+                                fontSize = 14.sp,
+                                fontStyle = FontStyle.Normal,
+                                fontWeight = FontWeight.Bold,
+                                fontFamily = FontFamily.SansSerif,
+                                textAlign = if (trackNameLen > 18) {
+                                    TextAlign.Left
+                                } else {
+                                    TextAlign.Center
+                                }
+                            ),
+                            modifier = GlanceModifier.size(width = 150.dp, height = 20.dp)
+                                .padding(start = 12.dp)
+                        )
+                        Spacer(modifier = GlanceModifier.size(width = 0.dp, height = 4.dp))
+                        Text(
+                            text = track.getArtists(),
+                            maxLines = 1,
+                            style = TextStyle(
+                                color = ColorProvider(containerTextColor),
+                                fontSize = 12.sp,
+                                fontStyle = FontStyle.Italic,
+                                fontWeight = FontWeight.Medium,
+                                fontFamily = FontFamily.SansSerif,
+                                textAlign = if (artistsNameLen > 20) {
+                                    TextAlign.Start
+                                } else {
+                                    TextAlign.Center
+                                },
+                                textDecoration = TextDecoration.Underline
+                            ),
+                            modifier = GlanceModifier.size(width = 140.dp, height = 15.dp)
+                                .padding(start = 12.dp)
+                        )
+                        Spacer(modifier = GlanceModifier.size(width = 0.dp, height = 10.dp))
+                        Row(
+                            modifier = GlanceModifier.padding(start = 12.dp)
+                        ) {
+                            Image(
+                                provider = ImageProvider(R.drawable.prev_foreground),
+                                contentDescription = "Previous",
+                                modifier = GlanceModifier.size(40.dp)
+                                    .cornerRadius(50.dp)
+                                    .clickable {
+                                        spotifyHelper.handleAction(
+                                            action = "prev",
+                                            value = playerRestrictions.canSkipPrev.toString(),
+                                            loading = loading,
+                                            onSuccess = onSuccessAction,
+                                            onError = onErrorAction
+                                        )
+                                    },
+                                colorFilter = ColorFilter.tint(ColorProvider(iconColor))
+                            )
+                            Image(
+                                provider = ImageProvider(
+                                    if (isPaused) {
+                                        R.drawable.play_foreground
+                                    } else {
+                                        R.drawable.pause_foreground
+                                    }
+                                ),
+                                contentDescription = "Play/Pause",
+                                modifier = GlanceModifier.size(40.dp)
+                                    .cornerRadius(50.dp)
+                                    .clickable {
+                                        spotifyHelper.handleAction(
+                                            action = if (isPaused) {
+                                                "play"
+                                            } else {
+                                                "pause"
+                                            },
+                                            value = "",
+                                            loading = loading,
+                                            onSuccess = onSuccessAction,
+                                            onError = onErrorAction
+                                        )
+                                    },
+                                colorFilter = ColorFilter.tint(ColorProvider(iconColor))
+                            )
+                            Image(
+                                provider = ImageProvider(R.drawable.next_foreground),
+                                contentDescription = "Next",
+                                modifier = GlanceModifier.size(40.dp)
+                                    .cornerRadius(50.dp)
+                                    .clickable {
+                                        spotifyHelper.handleAction(
+                                            action = "next",
+                                            value = playerRestrictions.canSkipNext.toString(),
+                                            loading = loading,
+                                            onSuccess = onSuccessAction,
+                                            onError = onErrorAction
+                                        )
+                                    },
+                                colorFilter = ColorFilter.tint(ColorProvider(iconColor))
+                            )
+
+                        }
+
+                    }
+                }
+
+            }
+            Box(
+                modifier = GlanceModifier.fillMaxSize(),
+                contentAlignment = Alignment.TopEnd
+            ) {
+                Image(
+                    provider = ImageProvider(R.drawable.spotify),
+                    contentDescription = "Spotify logo",
+                    modifier = GlanceModifier.size(12.dp)
+                )
+            }
+
+        }
+    }
+}
